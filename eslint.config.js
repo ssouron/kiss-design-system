dcc384604837110fa9c1994075520ea4925dc3c7
Stéphane Souron <stephane.souron@gmail.com>
2025-01-06 20:56:48 +0100

    add and setup prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,19 @@
-import globals from "globals"
-import pluginJs from "@eslint/js"
-import tseslint from "typescript-eslint"
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { ignores: ["node_modules", "dist"] },
-  { languageOptions:
-    { globals: globals.browser }
+  eslintConfigPrettier,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { ignores: ['node_modules', 'dist'] },
+  { languageOptions: { globals: globals.browser } },
+  {
+    rules: {
+      'no-console': 'warn',
+      'prefer-const': ['error', { ignoreReadBeforeAssign: true }],
+    },
   },
-  { rules: {
-      "no-console": "warn",
-      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
-    }
-  }
 ]
---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  proseWrap: 'always',
+}
